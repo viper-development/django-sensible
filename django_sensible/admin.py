1bc8ddaf0264e470313225bdbb34142229baf9d1
Romain Gonçalves <commit@rgoncalves.se>
2021-08-16 10:09:00 +0200

    admin.py: Editable and sortable choice fields
    CharField that uses choice inputs can be sorted and edited from the
    display list.

--- django_sensible/admin.py
+++ django_sensible/admin.py
@@ -1,6 +1,39 @@
 from django.contrib import admin
 from django.db import models
 
+
+def is_field_choice_input(field):
+    """
+    Return if a field is a CharField with enforced choice input.
+    """
+    return isinstance(field, models.CharField) and field.choices
+
+
+def is_field_filter(field):
+    """
+    Return true if a field is suitable for filtering in display list.
+    """
+    valid_models = [
+        models.fields.BooleanField,
+        models.ForeignKey,
+        models.ManyToManyField
+    ]
+
+    # Choice fields can be used as a filter
+    # (There is a limited number of possible values).
+    return (any(isinstance(field, model) for model in valid_models)
+            or is_field_choice_input(field))
+
+
+def is_field_editable(field):
+    """
+    Return true if a field is suitable for edition in display list
+    (Essentially boolean fields, thanks for their tiny widget).
+    """
+    return (is_field_choice_input(field)
+            or isinstance(field, models.BooleanField))
+
+
 class ModelAdmin(admin.ModelAdmin):
     """
     Generic ModelAdmin with shortcuts.
@@ -15,14 +48,8 @@ class ModelAdmin(admin.ModelAdmin):
         """
         Build a list of filters, composed of relationnal and boolean fields.
         """
-        return [
-            field.name for field in self.get_model_fields()
-            if any(isinstance(field, x) for x in [
-                models.fields.BooleanField,
-                models.ForeignKey,
-                models.ManyToManyField
-            ]) and field.unique is False
-        ]
+        return [field.name for field in self.get_model_fields()
+                if is_field_filter(field)]
 
     def get_list_display(self, request):
         """
@@ -35,7 +62,7 @@ class ModelAdmin(admin.ModelAdmin):
         From the list display, those fields can be edited and saved.
         """
         return [field.name for field in self.get_model_fields()
-                if isinstance(field, models.fields.BooleanField)]
+                if is_field_editable(field)]
 
     def get_changelist_instance(self, request):
         """
